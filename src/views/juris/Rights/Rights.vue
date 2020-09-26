778ba380a4751c7ed0bf3e9b45df0ad248a7fd0c
HW0001 <1600860624@qq.com>
2020-09-26 18:37:45 +0800

    增加打包wenj

--- src/views/juris/Rights/Rights.vue
+++ src/views/juris/Rights/Rights.vue
@@ -1,39 +1,31 @@
 <!--  -->
 <template>
-<div>
+  <div>
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>权限管理</el-breadcrumb-item>
     </el-breadcrumb>
     <el-card class="box-card">
       <rights-table />
-      </el-card> 
-</div>
+    </el-card>
+  </div>
 </template>
 
 <script>
-import RightsTable from "./RightsTable"
+import RightsTable from "./RightsTable";
 export default {
-data() {
-return {
-
-}
-},
-components:{
-  RightsTable
-}
-,
-//生命周期 - 创建完成（访问当前this实例）
-created() {
-
-},
-//生命周期 - 挂载完成（访问DOM元素）
-mounted() {
-
-}
-}
+  data() {
+    return {};
+  },
+  components: {
+    RightsTable,
+  },
+  //生命周期 - 创建完成（访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（访问DOM元素）
+  mounted() {},
+};
 </script>
 <style scoped>
 /* @import url(); 引入css类 */
-
 </style>
--- src/views/juris/Rights/RightsTable.vue
+++ src/views/juris/Rights/RightsTable.vue
@@ -6,19 +6,21 @@
       <el-table-column prop="path" label="路径"></el-table-column>
       <el-table-column label="权限等级">
         <template slot-scope="scope">
-         <el-button :type="btntype[scope.row.level]" plain disabled>第{{btntxt[scope.row.level]}}级</el-button>
+          <el-button :type="btntype[scope.row.level]" plain disabled
+            >第{{ btntxt[scope.row.level] }}级</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
-      <el-pagination
-    @size-change="handleSizeChange"
-    @current-change="handleCurrentChange"
-    :current-page="pageinfo.pagenum"
-    :page-sizes="[5, 10, 15,20]"
-    :page-size="pageinfo.pagesize"
-    layout="total, sizes, prev, pager, next, jumper"
-    :total="allrights.length"
-  ></el-pagination>
+    <el-pagination
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="pageinfo.pagenum"
+      :page-sizes="[5, 10, 15, 20]"
+      :page-size="pageinfo.pagesize"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="allrights.length"
+    ></el-pagination>
   </div>
 </template>
 
@@ -29,20 +31,12 @@ export default {
     return {
       rights: [],
       allrights: [],
-      pageinfo:{
-          pagesize:10,
-          pagenum:1
+      pageinfo: {
+        pagesize: 10,
+        pagenum: 1,
       },
-      btntype:[
-          "primary",
-          "success",
-          "warning"
-      ] ,
-      btntxt:[
-          "一",
-          "二",
-          "三"
-      ]
+      btntype: ["primary", "success", "warning"],
+      btntxt: ["一", "二", "三"],
     };
   },
   methods: {
@@ -51,26 +45,26 @@ export default {
       if (result.meta.status != 200)
         return this.$Message.error("获取权限列表数据失败！");
       this.allrights = result.data;
-      this.getPaginRights()
+      this.getPaginRights();
     },
-    getPaginRights(){
-        this.rights=[]
-        let starindex = (this.pageinfo.pagenum-1) * this.pageinfo.pagesize 
-        let endindex = this.pageinfo.pagenum * this.pageinfo.pagesize - 1
-        if(endindex>=this.allrights.length)endindex=this.allrights.length-1
-        for(let i=starindex ; i<= endindex ; i++)
-        {
-           this.rights.push(this.allrights[i])
-        }
+    getPaginRights() {
+      this.rights = [];
+      let starindex = (this.pageinfo.pagenum - 1) * this.pageinfo.pagesize;
+      let endindex = this.pageinfo.pagenum * this.pageinfo.pagesize - 1;
+      if (endindex >= this.allrights.length)
+        endindex = this.allrights.length - 1;
+      for (let i = starindex; i <= endindex; i++) {
+        this.rights.push(this.allrights[i]);
+      }
     },
-    handleSizeChange(size){
-        this.pageinfo.pagesize=size
-        this.getPaginRights() 
+    handleSizeChange(size) {
+      this.pageinfo.pagesize = size;
+      this.getPaginRights();
     },
-    handleCurrentChange(pagenum){
-        this.pageinfo.pagenum=pagenum
-        this.getPaginRights() 
-    }
+    handleCurrentChange(pagenum) {
+      this.pageinfo.pagenum = pagenum;
+      this.getPaginRights();
+    },
   },
   //生命周期 - 创建完成（访问当前this实例）
   created() {
